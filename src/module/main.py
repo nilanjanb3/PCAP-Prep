e29beee2584836168d98f1e822acdf40edca0835
Nilanjan Bhattacharjee <nilanjan.bhattacharjee77@gmail.com>
2023-10-31 01:16:40 +0530

    added notes

--- src/module/main.py
+++ src/module/main.py
@@ -1,5 +1,9 @@
 from sys import path
 path.append('mod1')
 
-import mod1.module1
+import mod1.module1 as m1
+
+print(m1.a)
+print(m1._a)
+print(m1.__a)
 
--- src/module/mod1/module1.py
+++ src/module/mod1/module1.py
@@ -2,3 +2,7 @@ if __name__ == '__main__':
     print("This is module 1")
 
 print(__name__)
+
+a = 10
+_a = 100
+__a = 1000
